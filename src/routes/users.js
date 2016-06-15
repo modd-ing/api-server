1b72d80887eed082b11917a8c4539e9f1ffa2baa
Ante Sepic <antesepic@gmail.com>
2016-06-16 01:17:10 +0200

    Add initial GET routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,7 @@
 const Promise = require( 'bluebird' );
 const seneca = require( '../seneca' )();
 const _ = require( 'lodash' );
+const routesUtil = require( '../util/routes-util' );
 
 // Promisify the seneca .act() method
 const act = Promise.promisify( seneca.act, { context: seneca });
@@ -14,7 +15,88 @@ module.exports = function( server ) {
   server
     .get( routeBase, function( req, res, next ) {
 
-      res.json({});
+      act({
+				role: 'api',
+				path: 'users',
+				type: 'read',
+				cmd: 'get',
+				params: req.params,
+				query: req.query
+				})
+				.then( ( reply ) => {
+
+					let status = 200,
+            payload = {
+              data: reply.data
+            };
+
+          if ( ! _.isEmpty( reply.errors ) ) {
+
+            payload = {
+              errors: reply.errors
+            };
+
+            status = routesUtil.extractAppropriateStatus( reply.errors );
+
+          } else if ( _.isEmpty( reply.data ) ) {
+
+						status = 404;
+
+					}
+
+					res
+						.status( status )
+						.json( payload );
+
+				})
+				.catch( ( err ) => {
+
+					res.sendStatus( 500 );
+
+				});
+
+    })
+    .get( routeBase + '/:id', function( req, res, next ) {
+
+      act({
+				role: 'api',
+				path: 'users',
+				type: 'read',
+				cmd: 'get',
+				params: req.params,
+				query: req.query
+				})
+				.then( ( reply ) => {
+
+					let status = 200,
+            payload = {
+              data: reply.data
+            };
+
+          if ( ! _.isEmpty( reply.errors ) ) {
+
+            payload = {
+              errors: reply.errors
+            };
+
+            status = routesUtil.extractAppropriateStatus( reply.errors );
+
+          } else if ( _.isEmpty( reply.data ) ) {
+
+						status = 404;
+
+					}
+
+					res
+						.status( status )
+						.json( payload );
+
+				})
+				.catch( ( err ) => {
+
+					res.sendStatus( 500 );
+
+				});
 
     });
 
