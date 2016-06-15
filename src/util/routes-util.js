5965ebfb89fed95edd305622cb184f71ac4c2fcf
Ante Sepic <antesepic@gmail.com>
2016-06-16 01:15:22 +0200

    Add routes utility module

---
+++ src/util/routes-util.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const _ = require( 'lodash' );
+
+/**
+ * Given an array of objects with an html status, it returns
+ * the most fitting one based on the first object.
+ * @param {array} objects - Array of objects with the status property.
+ */
+exports.extractAppropriateStatus = function( objects ) {
+
+  let defaultStatus = 500;
+
+  if ( ! _.isArray( objects ) ) {
+
+    return defaultStatus;
+
+  }
+
+  objects = objects.filter( ( object ) => {
+    return _.isInteger( object.status );
+  });
+
+  if ( _.isEmpty( objects ) ) {
+
+    return defaultStatus;
+
+  }
+
+  let firstObject = _.first( objects ),
+    firstStatus = firstObject.status;
+
+  if ( 1 === objects.length ) {
+
+    return firstStatus;
+
+  }
+
+  function statusIsEqualToFirstStatus( object ) {
+
+    return object.status === firstStatus;
+
+  }
+
+  if ( objects.every( statusIsEqualToFirstStatus ) ) {
+
+    return firstStatus;
+
+  }
+
+  if ( firstStatus >= 500 ) {
+
+    return 500;
+
+  } else if ( firstStatus >= 400 ) {
+
+    return 400;
+
+  } else if ( firstStatus >= 300 ) {
+
+    return 300;
+
+  }
+
+  return defaultStatus;
+
+}
