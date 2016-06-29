cffbc3d014482703c37d0de0a0f317f6e1c01f95
Ante Sepic <antesepic@gmail.com>
2016-06-30 00:17:23 +0200

    Implement express-session

--- src/index.js
+++ src/index.js
@@ -8,11 +8,34 @@ const bodyParser = require( 'body-parser' );
 const compress = require( 'compression' );
 const requestIp = require( 'request-ip' );
 const cookieParser = require( 'cookie-parser' );
+const session = require( 'express-session' );
+const RedisStore = require( 'connect-redis' )( session );
 
 const server = express();
 
-// Load server settings
-server.set( 'cookieSecret', process.env.COOKIE_SECRET );
+// We use sessions
+server.use( session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.SESSION_SECRET,
+  store: new RedisStore({
+    host: 'redis-api'
+  }),
+  ttl: 86400 * 7 // 7 days
+}));
+
+// Check if session was initialized correctly
+server.use( ( req, res, next ) => {
+
+  if ( ! req.session ) {
+
+    process.exit( 1 );
+
+  }
+
+  next();
+
+});
 
 // GZip all the things
 server.use( compress() );
@@ -21,7 +44,7 @@ server.use( compress() );
 server.use( bodyParser.json() );
 
 // Parse cookies
-server.use( cookieParser( server.get( 'cookieSecret' ) ) );
+server.use( cookieParser( process.env.COOKIE_SECRET ) );
 
 // Set the client ip
 server.use( function( req, res, next ) {
