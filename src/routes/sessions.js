bea7b550e4e558838b917d873768a2eeb70cb672
Ante Sepic <antesepic@gmail.com>
2016-07-01 00:34:34 +0200

    Add user role to the consumer session property

--- src/routes/sessions.js
+++ src/routes/sessions.js
@@ -198,10 +198,16 @@ module.exports = function( server ) {
 
           }
 
-          req.session.consumer = user.id;
+          req.session.consumer = {
+            id: user.id,
+            role: user.role
+          };
 
           jwt.sign(
-            { id: user.id },
+            {
+              id: user.id,
+              role: user.role
+            },
             process.env.JWT_SECRET,
             { expiresIn: '7d' },
             ( err, token ) => {
